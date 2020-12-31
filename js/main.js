60fe84cf18a39f52ec00890e54befa1c2109cf91
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-12-31 16:15:58 +0200

    main.js updates

--- js/main.js
+++ js/main.js
@@ -38,21 +38,38 @@ function sortData (list){
 
 function renderTable(monthNames, cashFlow) {
     let HTML =''; 
+        balance = 0;
+        income = 0;
+        expense = 0;
+
     cashFlow = sortData(cashFlow);
 
     for(let i=0; i<cashFlow.length; i++) {
         const item = cashFlow[i];
+        income += item.income ? item.income : 0;
+        expense += item.expense? item.expense : 0;
+        balance = income - expense;
+
+        // balance -= item.expense? item.expense : 0; sita darom tam, kad kai sutinka ne skaiciu o -, tada kad ji skaitytu kaip 0, nes kitaip tampa undefined ir toliau neskaiciuoja.
+
 
         HTML += `<div class="table-row">
                     <div class="cell">${i+1}</div>
                     <div class="cell">${monthNames[item.month-1]}</div>
                     <div class="cell">${formatMoney(item.income)} </div>
                     <div class="cell">${formatMoney(item.expense)} </div>
-                    <div class="cell">${formatMoney(0)}</div>
+                    <div class="cell">${formatMoney(balance)}</div>
                 </div>`
     }
     const tableContentDOM = document.querySelector('.table-content');
+    const footerIncomeDOM = document.querySelector('.table-footer > .cell:nth-of-type(3)');
+    const footerExpenseDOM = document.querySelector('.table-footer > .cell:nth-of-type(4)');
+    const footerBalanceDOM = document.querySelector('.table-footer > .cell:nth-of-type(5)');
     tableContentDOM.innerHTML = HTML;
+
+    footerIncomeDOM.innerText = formatMoney(income); 
+    footerExpenseDOM.innerText = formatMoney(expense); 
+    footerBalanceDOM.innerText = formatMoney(balance); 
 
 
 
